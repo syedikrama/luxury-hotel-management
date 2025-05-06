3cfde2d4e1b26a553303af28033c518755d6073a
AmmarKotwal <123722868+AmmarKotwal@users.noreply.github.com>
2025-05-06 11:10:25 +0500

    first commit
    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
